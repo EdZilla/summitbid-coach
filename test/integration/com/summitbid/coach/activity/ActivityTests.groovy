6b621bd6c0ff1f59dc87d3196aacce0d00fe7a51
Ed Young <ejy@summitbid.com>
2011-04-30 23:13:14 -0600

    adding ActivityTests and FoodTests

---
+++ test/integration/com/summitbid/coach/activity/ActivityTests.groovy
@@ -0,0 +1,17 @@
+package com.summitbid.coach.activity
+
+import grails.test.*
+
+class ActivityTests extends GroovyTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
